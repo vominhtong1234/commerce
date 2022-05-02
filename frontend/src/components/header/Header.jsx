0500f878149f33c2bb430930f30808d1392a902c
Vo Minh Tong <vominhtong.tn@gmail.com>
2022-05-02 08:05:11 +0700

    Add header folder

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className='header'>
+			<nav>
+				<li>
+					<a href='/'>Home</a>
+				</li>
+			</nav>
+		</div>
+	);
+};
+
+export default Header;
